a67e24b6b6508d963e753af60476a5960921a1f3
Smarth Bajaj <smarthbajaj28@gmail.com>
2020-02-16 20:16:44 +1100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseList from "./Components/ExpenseList";
+import ExpenseForm from "./Components/ExpenseForm";
+import Alert from "./Components/Alert";
+import uuid from "uuid/v4";
+
+const initialExpenses = [
+  {
+    id: uuid(),
+    charge: "rent",
+    amount: 1600
+  },
+  {
+    id: uuid(),
+    charge: "bike",
+    amount: 2000
+  },
+  {
+    id: uuid(),
+    charge: "car",
+    amount: 1000
+  }
+];
+
+function App() {
+  //const result = useState(initialExpenses);
+  // const expenses = result[0];
+  // const setExpenses = result[1];
+  // console.log(expenses, setExpenses);
+  // or
+
+  // state values
+  // all expenses, add expenses
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+
+  //alert
+  const [alert, setAlert] = useState({ show: false });
+
+  // functionality
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = e => {
+    setAmount(e.target.value);
+  };
+
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 3000);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpenses = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpenses]);
+      handleAlert({ type: "success", text: "item added" });
+      setCharge("");
+      setAmount("");
+    } else {
+      // handle alert call
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empty value and amount has to be bigger than zero`
+      });
+    }
+  };
+
+  //clear all items
+  const clearItems = () => {
+    setExpenses([]);
+  };
+
+  //handle delete
+  const handleDelete = id => {};
+
+  //handle edit
+  const handleEdit = id => {
+    console.log(`item edited: ${id}`);
+  };
+
+  return (
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </main>
+      <h1>
+        total spending:{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += parseInt(curr.amount));
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
